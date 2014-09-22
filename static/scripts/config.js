b12bde3b554730913546cd612ca0367cb795a5b6
Santiago Lizardo <santiago.lizardo@gmail.com>
2014-09-22 21:35:28 +0100

    requirejs added to the game. config variables come from an external js

---
+++ static/scripts/config.js
@@ -0,0 +1,7 @@
+define({
+	'moves': 10,
+	'points': 0,
+	'max_number': 2,
+	'bricks': 24,
+	'brick_size': 40,
+});
---
+++ static/scripts/main.js
@@ -0,0 +1,10 @@
+
+require.config({
+	baseUrl: 'scripts/',
+	paths: {
+		jquery: 'jquery-1.11.1.min'
+	}
+});
+
+require( [ 'index' ] );
+
