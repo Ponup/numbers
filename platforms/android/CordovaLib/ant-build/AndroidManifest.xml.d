81eeace1c878b5485380202a732b5987ab327865
Santiago Lizardo <santiago.lizardo@gmail.com>
2014-09-29 21:47:14 +0100

    Cordova files removed. Packaging is going to be part of another repository

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/pablolizardo/sources/soki.numbers/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/pablolizardo/sources/soki.numbers/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/Numbers-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/pablolizardo/sources/soki.numbers/platforms/android/ant-build/Numbers-debug-unaligned.apk : \
-/home/pablolizardo/sources/soki.numbers/platforms/android/ant-build/Numbers.ap_ \
-/home/pablolizardo/sources/soki.numbers/platforms/android/ant-build/classes.dex \
-/home/pablolizardo/sources/soki.numbers/platforms/android/CordovaLib/ant-build/classes.jar \
